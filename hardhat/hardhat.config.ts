ea49823b354e5caaac9d482f059325b0694654ae
WiFoDev <lou81804@gmail.com>
2022-11-01 16:44:12 -0500

    add deploy script and configuration

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import {config as envConfig} from 'dotenv'
+envConfig()
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
