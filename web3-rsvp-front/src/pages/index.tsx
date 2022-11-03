ff8fe4f647b1f27c2ddb9d76cb0bb30f18e803f8
WiFoDev <lou81804@gmail.com>
2022-11-02 22:39:57 -0500

    setup frontend project

---
+++ web3-rsvp-front/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type {NextPage} from "next";
+
+import {Layout} from "@/Layout";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <section>Welcome</section>
+    </Layout>
+  );
+};
+
+export default Home;
